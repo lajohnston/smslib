ab1c6c7456d7b8fc649c2366c896ae6dc41297ac
lajohnston <lajohnston@live.co.uk>
2020-04-10 16:58:12 +0100

    Clarify palette docs

--- palette.asm
+++ palette.asm
@@ -1,11 +1,11 @@
 ;====
 ; Color palette
 ;
-; The color palette consists of 32 slots. Each color is a byte
+; The color palette consists of 32 slots (0-31). Each color is a byte
 ; containing 2-bit RGB colors in the format (--bbggrr).
 ;
-; Background tiles/patterns can use either the first 16 slots, or the
-; last 16 slots. Sprites can only use the last 16 slots
+; Background tiles/patterns can use either the first 16 slots (0-15), or the
+; last 16 slots (16-31). Sprites can only use the last 16 slots (16-31).
 ;====
 
 ;====
@@ -18,17 +18,17 @@
 ;====
 ; Set a range of colors
 ;
-; @in    fromSlot        the first color slot to set
-; @in    numberOfSlots   the number of slots to set
-; @in    dataAddress     the address the points to the data to load. Each colour
+; @in    fromSlot        the first color slot to set (0-31)
+; @in    dataAddr        the address of the data to load. Each color
 ;                        should be a byte containing an RGB value in the format
 ;                        --bbggrr
+; @in    dataSize        the data size in bytes/the number of slots to set
 ; @clobs af, bc, hl
 ;====
-.macro "palette.set" args fromSlot numberOfSlots dataAddress
+.macro "palette.set" args fromSlot dataAddr dataSize
     smslib.setVdpWrite palette.address + fromSlot
-    ld hl, dataAddress
-    ld b, numberOfSlots
+    ld hl, dataAddr
+    ld b, dataSize
     ld c, vdp.DATA_PORT
     otir
 .endm
