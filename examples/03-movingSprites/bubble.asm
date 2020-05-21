a29de89788b127f2ab3f359900a1bf09251dd60a
lajohnston <lajohnston@live.co.uk>
2020-05-21 19:17:13 +0100

    Add input.asm to moveingSprites example

---
+++ examples/03-movingSprites/bubble.asm
@@ -0,0 +1,124 @@
+;====
+; A 'bubble' entity that can be controlled with the joypad direction buttons
+;====
+
+;====
+; Define Bubble struct
+;====
+.struct "Bubble"
+    xPos:   db  ; the current x position
+    yPos:   db  ; the current y position
+    xVec:   db  ; the number of x pixels to move per frame
+    yVec:   db  ; the number of y pixels to move per frame
+.endst
+
+;====
+; Initialises a bubble instance in RAM
+;
+; @in   ix  pointer to bubble
+;====
+.macro "bubble.init"
+    ld (ix + Bubble.xVec), 0
+    ld (ix + Bubble.yVec), 0
+    ld (ix + Bubble.xPos), 100
+    ld (ix + Bubble.yPos), 80
+.endm
+
+;====
+; Updates the bubble's movement vector based on the joypad input
+;
+; @in   ix  pointer to bubble
+;====
+.section "bubble.updateInput" free
+    bubble.updateInput:
+        ; Read the port 1 into register b
+        input.setRegister "c"
+        input.readPort1
+
+        ; Update xVec
+        xor a                       ; xVec = 0
+        input.if input.LEFT, +
+            ld a, -1                ; xVec = -1
+        +:
+        input.if input.RIGHT, +
+            ld a, 1                 ; xVec = 1
+        +:
+        ld (ix + Bubble.xVec), a    ; store xVec
+
+        ; Update yVec
+        xor a                       ; yVec = 0
+        input.if input.UP, +
+            ld a, -1                ; yVec = -1
+        +:
+        input.if input.DOWN, +
+            ld a, 1                 ; yVec = 1
+        +:
+        ld (ix + Bubble.yVec), a    ; store yVec
+
+        ret
+.ends
+
+;====
+; Updates the bubble's position based on its movement vector
+;
+; @in   ix  pointer to bubble
+;====
+.section "bubble.updateMovement" free
+    bubble.updateMovement:
+        ; xPos = xPos + yVec
+        ld a, (ix + Bubble.xPos)    ; load xPos
+        add a, (ix + Bubble.xVec)   ; add xVec to xPos
+        ld (ix + Bubble.xPos), a    ; store in xPos
+
+        ; yPos = yPos + yVec
+        ld a, (ix + Bubble.yPos)    ; load yPos
+        add a, (ix + Bubble.yVec)   ; add yVec to yPos
+        ld (ix + Bubble.yPos), a    ; store in yPos
+        ret
+.ends
+
+;====
+; Adds the bubble's sprites to the sprite buffer
+;
+; @in   ix  pointer to bubble
+;====
+.section "bubble.updateSprite"
+    bubble.updateSprite:
+        ; Add sprite group to sprite buffer
+        sprites.setSlot 0
+        ld hl, bubble.spriteGroup
+        ld b, (ix + Bubble.xPos)    ; base x pos
+        ld c, (ix + Bubble.yPos)    ; base y pos
+        sprites.addGroup            ; add group
+        ret
+.ends
+
+;====
+; Assets
+;====
+.section "bubble assets" free
+    bubble.palette:
+        .db $00 $11 $22 $32 $36 $3F
+
+    bubble.patterns:
+        ; Tile index $000
+        .db $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00
+        ; Tile index $001
+        .db $08 $07 $00 $00 $04 $1C $03 $00 $10 $30 $0F $00 $20 $60 $1F $00 $80 $40 $3F $00 $40 $C0 $3F $00 $00 $80 $7F $00 $00 $80 $7F $00
+        ; Tile index $002
+        .db $10 $E0 $00 $00 $20 $38 $C0 $00 $08 $0C $F0 $00 $64 $06 $F8 $00 $11 $02 $FC $00 $0A $03 $FC $00 $08 $01 $FE $00 $00 $01 $FE $00
+        ; Tile index $003
+        .db $00 $80 $7F $00 $00 $80 $7F $00 $40 $C0 $3F $00 $80 $40 $3F $00 $20 $60 $1F $00 $10 $30 $0F $00 $04 $1C $03 $00 $08 $07 $00 $00
+        ; Tile index $004
+        .db $00 $01 $FE $00 $00 $01 $FE $00 $02 $03 $FC $00 $01 $02 $FC $00 $04 $06 $F8 $00 $08 $0C $F0 $00 $20 $38 $C0 $00 $10 $E0 $00 $00
+        ; Tile index $005
+        .db $42 $3C $00 $00 $81 $42 $3C $00 $08 $81 $7E $00 $04 $81 $7E $00 $00 $81 $7E $00 $00 $81 $7E $00 $81 $42 $3C $00 $42 $3C $00 $00
+
+    bubble.spriteGroup:
+        ; pattern, relativeX, relativeY
+        sprites.sprite 1, 0, 0  ; top left      (x0, y0)
+        sprites.sprite 2, 8, 0  ; top right     (x+8, y0)
+        sprites.sprite 3, 0, 8  ; bottom left   (x0, y+8)
+        sprites.sprite 4, 8, 8  ; bottom right  (x+8, y+8)
+        sprites.endGroup
+.ends
--- examples/03-movingSprites/main.asm
+++ examples/03-movingSprites/main.asm
@@ -1,7 +1,8 @@
 ;====
-; SMSLib Static sprites example
+; SMSLib moving sprites example
 ;
-; Moves sprites on the screen using a VBlank handler
+; Create a sprite the can be controller with the directional input of joypad 1.
+; Demonstrates VBlank handling using interrupts.asm
 ;====
 .sdsctag 1.10, "smslib sprites", "smslib static sprite tutorial", "lajohnston"
 
@@ -11,15 +12,25 @@
 .incdir "../../"            ; back to smslib directory
 .include "smslib.asm"       ; base library
 .include "mapper/basic.asm" ; memory mapper
+.include "input.asm"        ; handles input
 .include "palette.asm"      ; handles colors
 .include "patterns.asm"     ; handles patterns (tile images)
 .include "pause.asm"        ; handles pause button
 .include "sprites.asm"      ; handles a sprite buffer in RAM
 .include "vdpreg.asm"       ; handles vdp settings
 
-; Handle frame interrupts
+;====
+; Handle frame (VBlank) interrupts. This will call the interrupts.vBlank label
+; below each time a frame has finished being drawn. This occurs 50 times a
+; second for PAL and 60 times per second for NTSC and can be used to regulate
+; the logic speed
+;====
 .define interrupts.handleVBlank 1
 .include "interrupts.asm"
+
+; Import bubble entity
+.incdir "."
+.include "bubble.asm"
 
 ;====
 ; Boot sequence at ROM address 0
@@ -31,7 +42,7 @@
 .ends
 
 ;====
-; Place an instance of sprite.Buffer somewhere in RAM with an offset of $40
+; Place an instance of sprites.Buffer somewhere in RAM with an offset of $40
 ; This offset is used by smslib to perform optimisations
 ;====
 .ramsection "ram" bank 0 slot mapper.RAM_SLOT orga $C040 force
@@ -39,83 +50,72 @@
 .ends
 
 ;====
-; Define a Bubble struct containing xy position and movement vectors
-;====
-.struct "Bubble"
-    xVec:   db
-    xPos:   db
-    yVec:   db
-    yPos:   db
-.endst
-
-;====
 ; Reserve a place in RAM for a Bubble instance
 ;====
 .ramsection "bubble" slot mapper.RAM_SLOT
-    bubble: instanceof Bubble
+    bubbleInstance: instanceof Bubble
 .ends
 
 ;====
-; Initialise program
+; Initialise the example
 ;====
-.section "code" free
+.section "init" free
     init:
         ; Load sprite palette
         palette.setSlot palette.SPRITE_PALETTE
-        palette.load paletteData, 6
+        palette.load bubble.palette, 6
 
-        ; Load pattern data into slots 256+ (used by sprites, by default)
+        ; Load pattern data to draw sprites. Sprites use slots 256+ by default
         patterns.setSlot 256
-        patterns.load patternData, 6
+        patterns.load bubble.patterns, 6
 
-        ; Initialise bubble
-        ld ix, bubble
-        ld (ix + Bubble.xVec), 0
-        ld (ix + Bubble.yVec), -1
-        ld (ix + Bubble.xPos), 100
-        ld (ix + Bubble.yPos), 80
+        ; Initialise bubble with default values
+        ld ix, bubbleInstance
+        bubble.init
 
         ; Enable the display and interrupts
         vdpreg.startBatch
-        vdpreg.enableDisplay
-        vdpreg.enableVBlank
+            vdpreg.enableDisplay
+            vdpreg.enableVBlank
+            vdpreg.hideLeftColumn
         vdpreg.endBatch
 
+        ; Begin
         interrupts.enable
+        jp mainLoop
+.ends
 
+;====
+; Update bubble each frame
+;====
+.section "mainLoop" free
     mainLoop:
-        ; Wait for VBlank to finish before continuing
+        ; Wait for frame interrupt handler to finish before continuing
         interrupts.waitForVBlank
 
-        ; If paused, keep restarting loop until unpaused
-        pause.jpIfPaused mainLoop
-
-        ; Add bubble movement vector to position
-        ld ix, bubble
-
-        ; Add xVec to xPos
-        ld a, (ix + Bubble.xPos)    ; load xPos
-        add a, (ix + Bubble.xVec)   ; add xVec to xPos
-        ld (ix + Bubble.xPos), a    ; store in xPos
-
-        ; Add yVec to yPos
-        ld a, (ix + Bubble.yPos)    ; load yPos
-        add a, (ix + Bubble.yVec)   ; add yVec to yPos
-        ld (ix + Bubble.yPos), a    ; store in yPos
-
-        ; Add sprite group to sprite buffer
-        sprites.setSlot 0
-        ld hl, spriteGroup
-        ld b, (ix + Bubble.xPos)    ; base x pos
-        ld c, (ix + Bubble.yPos)    ; base y pos
-        sprites.addGroup
-
-        sprites.end                 ; no more sprites
+        ; Update bubble instance
+        ld ix, bubbleInstance
+        call bubble.updateInput
+        call bubble.updateMovement
+        call bubble.updateSprite
+        sprites.end ; no more sprites
 
         ; Next loop
         jp mainLoop
+.ends
 
-    ; VBlank routine, called after each frame is rendered
+;====
+; VBlank routine called by interrupts.asm after each frame is drawn.
+;
+; This is a good time to send data to the VDP before it starts drawing the next
+; frame.
+;
+; When this has finished, the mainLoop will continue. The
+; interrupts.waitForVBlank will be satisfied and the rest of the loop will
+; proceed.
+;====
+.section "vBlankHandler" free
+    ; This is called by interrupts.asm
     interrupts.onVBlank:
         push bc
         push hl
@@ -123,35 +123,6 @@
         pop hl
         pop bc
 
+        ; End vBlank handler
         interrupts.endVBlank
-.ends
-
-;====
-; Assets
-;====
-.section "assets" free
-    paletteData:
-        .db $00 $11 $22 $32 $36 $3F
-
-    patternData:
-        ; Tile index $000
-        .db $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00 $00
-        ; Tile index $001
-        .db $08 $07 $00 $00 $04 $1C $03 $00 $10 $30 $0F $00 $20 $60 $1F $00 $80 $40 $3F $00 $40 $C0 $3F $00 $00 $80 $7F $00 $00 $80 $7F $00
-        ; Tile index $002
-        .db $10 $E0 $00 $00 $20 $38 $C0 $00 $08 $0C $F0 $00 $64 $06 $F8 $00 $11 $02 $FC $00 $0A $03 $FC $00 $08 $01 $FE $00 $00 $01 $FE $00
-        ; Tile index $003
-        .db $00 $80 $7F $00 $00 $80 $7F $00 $40 $C0 $3F $00 $80 $40 $3F $00 $20 $60 $1F $00 $10 $30 $0F $00 $04 $1C $03 $00 $08 $07 $00 $00
-        ; Tile index $004
-        .db $00 $01 $FE $00 $00 $01 $FE $00 $02 $03 $FC $00 $01 $02 $FC $00 $04 $06 $F8 $00 $08 $0C $F0 $00 $20 $38 $C0 $00 $10 $E0 $00 $00
-        ; Tile index $005
-        .db $42 $3C $00 $00 $81 $42 $3C $00 $08 $81 $7E $00 $04 $81 $7E $00 $00 $81 $7E $00 $00 $81 $7E $00 $81 $42 $3C $00 $42 $3C $00 $00
-
-    spriteGroup:
-        ; pattern, relativeX, relativeY
-        sprites.sprite 1, 0, 0  ; top left      (x0, y0)
-        sprites.sprite 2, 8, 0  ; top right     (x+8, y0)
-        sprites.sprite 3, 0, 8  ; bottom left   (x0, y+8)
-        sprites.sprite 4, 8, 8  ; bottom right  (x+8, y+8)
-        sprites.endGroup
 .ends
