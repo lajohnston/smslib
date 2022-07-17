d818636237738c62434cfcab7f7c2ec12c2f196d
Lee Johnston <lajohnston@live.co.uk>
2022-07-17 11:42:01 +0100

    Tilemap: Re-order public/private functions

--- tilemap.asm
+++ tilemap.asm
@@ -113,24 +113,8 @@
 .ends
 
 ;====
-; Reset the RAM buffers and scroll values to 0
+; Public functions
 ;====
-.macro "tilemap.reset"
-    ; Zero values
-    xor a   ; set A to 0
-    ld (tilemap.ram.xScrollBuffer), a
-    ld (tilemap.ram.flags), a
-    ld (tilemap.ram.yScrollBuffer), a
-
-    ld (tilemap.ram.vramRowWrite), a
-    ld (tilemap.ram.vramRowWrite + 1), a
-
-    ld (tilemap.ram.colWriteCall), a
-    ld (tilemap.ram.colWriteCall + 1), a
-
-    ; Zero scroll registers
-    tilemap.updateScrollRegisters
-.endm
 
 ;====
 ; Set the tile slot ready to write to
@@ -269,6 +253,13 @@
 .endm
 
 ;====
+; Alias for tilemap.loadRows
+;====
+.macro "tilemap.loadRows"
+    call tilemap.loadRows
+.endm
+
+;====
 ; Load tile data from an uncompressed map. Each tile is 2-bytes - the first is
 ; the tileRef and the second is the tile's attributes.
 ;
@@ -293,11 +284,230 @@
 .ends
 
 ;====
-; Alias for tilemap.loadRows
+; Reset/initialise the RAM buffers and scroll values to 0
 ;====
-.macro "tilemap.loadRows"
-    call tilemap.loadRows
+.macro "tilemap.reset"
+    ; Zero values
+    xor a   ; set A to 0
+    ld (tilemap.ram.xScrollBuffer), a
+    ld (tilemap.ram.flags), a
+    ld (tilemap.ram.yScrollBuffer), a
+
+    ld (tilemap.ram.vramRowWrite), a
+    ld (tilemap.ram.vramRowWrite + 1), a
+
+    ld (tilemap.ram.colWriteCall), a
+    ld (tilemap.ram.colWriteCall + 1), a
+
+    ; Zero scroll registers
+    tilemap.updateScrollRegisters
 .endm
+
+;====
+; Adjusts the buffered tilemap xScroll value by a given number of pixels. If this
+; results in a new column needing to be drawn it sets flags in RAM indicating
+; whether the left or right column needs reloading. You can interpret these flags
+; using tilemap.ifColScroll.
+;
+; The scroll value won't apply until you call tilemap.updateScrollRegisters
+;
+; @in   a   the number of x pixels to adjust. Positive values scroll right in
+;           the game world (shifting the tiles left). Negative values scroll
+;           left (shifting the tiles right)
+;====
+.macro "tilemap.adjustXPixels"
+    call tilemap._adjustXPixels
+.endm
+
+;====
+; Adjusts the buffered tilemap yScroll value by a given number of pixels. If this
+; results in a new row needing to be drawn it sets flags in RAM indicating
+; whether the top or bottom rows need reloading. You can interpret these flags
+; using tilemap.ifRowScroll.
+;
+; The scroll value won't apply until you call tilemap.updateScrollRegisters
+;
+; @in   a   the number of y pixels to adjust. Positive values scroll down in
+;           the game world (shifting the tiles up). Negative values scroll
+;           up (shifting the tiles down)
+;====
+.macro "tilemap.adjustYPixels"
+    call tilemap._adjustYPixels
+.endm
+
+;====
+; Calculates the adjustments made with tilemap.adjustXPixels/adjustYPixels
+; and applies them to the RAM variables
+;====
+.macro "tilemap.prepScroll"
+    call tilemap._prepScroll
+.endm
+
+;====
+; Point DE to rowBufferA in RAM, ready to write tile data to. RowBufferA stores tiles
+; from the left-most visible portion of the screen to the right-edge of the
+; tilemap (col 32).
+;
+; Also returns rowBufferA's length in bytes, i.e. the number of bytes you
+; should write to it (the number of tiles * 2). This is always > 0
+;
+; When writing a row of tiles, start by writing to rowBufferA until it's full
+; then write the remainder in rowBufferB (see tilemapsetRowBufferB).
+;
+; @out  de  pointer to rowBufferA in RAM
+; @out  a   rowBufferA length in bytes; the number of bytes to write to it
+;====
+.macro "tilemap.setRowBufferA"
+    ; Get offset to rowBufferA (CEILING(B2/4))
+    ld a, (tilemap.ram.xScrollBuffer)
+    neg             ; xScrollBuffer is negated - negate back
+    dec a           ; offset by 1 pixel so we'll get ceiling rather than floor
+    rrca            ; divide by 2
+    rrca            ; divide by 2 again (4 total); now equals col * 2 bytes
+    add 2           ; cancel offset we added earlier
+    and %00111110   ; clean value and get ceiling value
+
+    ; Set DE to rowBufferA
+    ld d, >tilemap.ram.rowBuffer    ; set D to high byte of rowBuffer
+    ld e, a                         ; set E to rowBufferA offset
+
+    ; Calculate number of bytes to write
+    ld a, tilemap.ROW_SIZE_BYTES
+    sub e                           ; subtract offset in E
+.endm
+
+;====
+; Point DE to rowBufferB in RAM. RowBufferB stores tiles from column 0 of the
+; tilemap to the left-edge of the visible screen.
+;
+; Also returns rowBufferB's length in bytes, i.e. the number of bytes you
+; should write to it (the number of tiles * 2); Unlike rowBufferA, rowBufferB
+; can have a length of zero - always check the Z flag before writing to it
+;
+; When writing a row of tiles, start by writing to rowBufferA until it's full
+; then write the remainder in rowBufferB
+;
+; @out  de  pointer to rowBufferB in RAM
+; @out  a   rowBufferB length in bytes; the number of bytes to write to it
+; @out  f   Z set if there is no data to write to rowBufferB
+;====
+.macro "tilemap.setRowBufferB"
+    ld de, tilemap.ram.rowBuffer    ; rowBufferB always point to beginning
+    ld a, (tilemap.ram.xScrollBuffer)
+    neg             ; xScrollBuffer is negated - negate back
+    dec a           ; offset by 1 pixel so we'll get ceiling rather than floor
+    rrca            ; divide by 2
+    rrca            ; divide by 2 again (4 total); now equals col * 2 bytes
+    add 2           ; cancel offset we added earlier
+    and %00111110   ; clean value and get ceiling value
+.endm
+
+;====
+; Jumps to the relevant label if a column scroll is needed after a call to
+; tilemap.adjustXPixels. Must be called with either 3 arguments (left, right, else)
+; or just else alone
+;
+; @in   left    (optional) continue to this label if left column needs loading
+; @in   right   (optional) jump to this label if the right column needs loading
+; @in   else    jump to this label if no columns need loading
+;====
+.macro "tilemap.ifColScroll" args left, right, else
+    .if NARGS == 1
+        ; Only one argument passed ('else' label)
+        ld a, (tilemap.ram.flags)               ; load flags
+        and tilemap.X_SCROLL_RESET_MASK ~ $ff   ; remove other flags (negate reset mask)
+        jp z, \1                                ; jp to else label if no column to scroll
+        ; ...otherwise continue
+    .elif NARGS == 3
+        ; 3 arguments passed (left, right, else)
+        ld a, (tilemap.ram.flags)               ; load flags
+        and tilemap.X_SCROLL_RESET_MASK ~ $ff   ; remove other flags (negate reset mask)
+        jp z, else                              ; no column to scroll
+
+        bit tilemap.SCROLL_RIGHT_PENDING_BIT, a ; check right scroll flag
+        jp nz, right                            ; jp if scrolling right
+        ; ...otherwise continue to left label
+    .else
+        .print "\ntilemap.ifColScroll requires 1 or 3 arguments (left/right/else, or just else alone)\n\n"
+        .fail
+    .endif
+.endm
+
+;====
+; Jumps to the relevant label if a row scroll is needed after a call to
+; tilemap.adjustXPixels. Must be called with either 3 arguments (up, down, else)
+; or just else alone
+;
+; @in   up      (optional) continue to this label if top row needs loading
+; @in   down    (optional) jump to this label if the bottom row needs loading
+; @in   else    jump to this label if no columns need loading
+;====
+.macro "tilemap.ifRowScroll" args up, down, else
+    .if NARGS == 1
+        ; Only one argument passed ('else' label)
+        ld a, (tilemap.ram.flags)               ; load flags
+        and tilemap.Y_SCROLL_RESET_MASK ~ $ff   ; remove other flags (negate reset mask)
+        jp z, \1                                ; jp to else label if no row to scroll
+        ; ...otherwise continue
+    .elif NARGS == 3
+        ld a, (tilemap.ram.flags)
+        and tilemap.Y_SCROLL_RESET_MASK ~ $ff   ; remove other flags (negate reset mask)
+        jp z, else                              ; no row to scroll
+
+        bit tilemap.SCROLL_DOWN_PENDING_BIT, a
+        jp nz, down                             ; scroll down
+        ; ...otherwise continue to 'up' label
+    .else
+        .print "\ntilemap.ifRowScroll requires 1 or 3 arguments (up/down/else, or just else alone)\n\n"
+        .fail
+    .endif
+.endm
+
+;====
+; Sends the buffered scroll register values to the VDP. This should be called
+; when the display is off or during a V or H interrupt
+;====
+.macro "tilemap.updateScrollRegisters"
+    ld a, (tilemap.ram.xScrollBuffer)
+    utils.vdp.setRegister utils.vdp.SCROLL_X_REGISTER
+
+    ld a, (tilemap.ram.yScrollBuffer)
+    utils.vdp.setRegister utils.vdp.SCROLL_Y_REGISTER
+.endm
+
+;====
+; Sets the VRAM write address to the row that requires updating. If no row needs
+; scrolling, the write address will be set to the last row scrolled
+;
+; @out  VRAM write address  write address for the row (column 0)
+; @out  c                   VDP data port
+;====
+.macro "tilemap.setRowScrollSlot"
+    ld hl, (tilemap.ram.vramRowWrite)
+    utils.vdp.prepWriteHL
+.endm
+
+;====
+; Set the VRAM write address to the column the requires updating
+;====
+.macro "tilemap.loadScrollCol"
+    ; Get X column offset
+    ld a, (tilemap.ram.xScrollBuffer)   ; load X scroll
+    neg                                 ; negate
+    rrca                                ; divide by 2
+    rrca                                ; divide by 2 (4)
+    and %00111110                       ; clean value
+    ld e, a                             ; load result into E
+
+    ld c, tilemap.VDP_DATA_PORT         ; data port
+    ld b, tilemap.COL_SIZE_BYTES        ; bytes to write
+    ld iy, (tilemap.ram.colWriteCall)   ; load call address in tilemap._loadColumn
+    call tilemap._callIY                ; call address
+.endm
+
+;====
+; Private/internal functions
+;====
 
 ;====
 ; See tilemap.adjustXPixels
@@ -454,38 +664,6 @@
 .ends
 
 ;====
-; Adjusts the buffered tilemap xScroll value by a given number of pixels. If this
-; results in a new column needing to be drawn it sets flags in RAM indicating
-; whether the left or right column needs reloading. You can interpret these flags
-; using tilemap.ifColScroll.
-;
-; The scroll value won't apply until you call tilemap.updateScrollRegisters
-;
-; @in   a   the number of x pixels to adjust. Positive values scroll right in
-;           the game world (shifting the tiles left). Negative values scroll
-;           left (shifting the tiles right)
-;====
-.macro "tilemap.adjustXPixels"
-    call tilemap._adjustXPixels
-.endm
-
-;====
-; Adjusts the buffered tilemap yScroll value by a given number of pixels. If this
-; results in a new row needing to be drawn it sets flags in RAM indicating
-; whether the top or bottom rows need reloading. You can interpret these flags
-; using tilemap.ifRowScroll.
-;
-; The scroll value won't apply until you call tilemap.updateScrollRegisters
-;
-; @in   a   the number of y pixels to adjust. Positive values scroll down in
-;           the game world (shifting the tiles up). Negative values scroll
-;           up (shifting the tiles down)
-;====
-.macro "tilemap.adjustYPixels"
-    call tilemap._adjustYPixels
-.endm
-
-;====
 ; Calculates the adjustments made with tilemap.adjustXPixels/adjustYPixels
 ; and applies them to the RAM variables.
 ;
@@ -585,72 +763,6 @@
 .ends
 
 ;====
-; Calculates the adjustments made with tilemap.adjustXPixels/adjustYPixels
-; and applies them to the RAM variables
-;====
-.macro "tilemap.prepScroll"
-    call tilemap._prepScroll
-.endm
-
-;====
-; Point DE to rowBufferA in RAM. RowBufferA stores tiles from the left-most
-; visible portion of the screen to the right-edge of the tilemap (col 32).
-;
-; Also returns rowBufferA's length in bytes, i.e. the number of bytes you
-; should write to it (the number of tiles * 2). This is always > 0
-;
-; When writing a row of tiles, start by writing to rowBufferA until it's full
-; then write the remainder in rowBufferB (see tilemapsetRowBufferB).
-;
-; @out  de  pointer to rowBufferA in RAM
-; @out  a   rowBufferA length in bytes; the number of bytes to write to it
-;====
-.macro "tilemap.setRowBufferA"
-    ; Get offset to rowBufferA (CEILING(B2/4))
-    ld a, (tilemap.ram.xScrollBuffer)
-    neg             ; xScrollBuffer is negated - negate back
-    dec a           ; offset by 1 pixel so we'll get ceiling rather than floor
-    rrca            ; divide by 2
-    rrca            ; divide by 2 again (4 total); now equals col * 2 bytes
-    add 2           ; cancel offset we added earlier
-    and %00111110   ; clean value and get ceiling value
-
-    ; Set DE to rowBufferA
-    ld d, >tilemap.ram.rowBuffer    ; set D to high byte of rowBuffer
-    ld e, a                         ; set E to rowBufferA offset
-
-    ; Calculate number of bytes to write
-    ld a, tilemap.ROW_SIZE_BYTES
-    sub e                           ; subtract offset in E
-.endm
-
-;====
-; Point DE to rowBufferB in RAM. RowBufferB stores tiles from column 0 of the
-; tilemap to the left-edge of the visible screen.
-;
-; Also returns rowBufferB's length in bytes, i.e. the number of bytes you
-; should write to it (the number of tiles * 2); Unlike rowBufferA, rowBufferB
-; can have a length of zero - always check the Z flag before writing to it
-;
-; When writing a row of tiles, start by writing to rowBufferA until it's full
-; then write the remainder in rowBufferB
-;
-; @out  de  pointer to rowBufferB in RAM
-; @out  a   rowBufferB length in bytes; the number of bytes to write to it
-; @out  f   Z set if there is no data to write to rowBufferB
-;====
-.macro "tilemap.setRowBufferB"
-    ld de, tilemap.ram.rowBuffer    ; rowBufferB always point to beginning
-    ld a, (tilemap.ram.xScrollBuffer)
-    neg             ; xScrollBuffer is negated - negate back
-    dec a           ; offset by 1 pixel so we'll get ceiling rather than floor
-    rrca            ; divide by 2
-    rrca            ; divide by 2 again (4 total); now equals col * 2 bytes
-    add 2           ; cancel offset we added earlier
-    and %00111110   ; clean value and get ceiling value
-.endm
-
-;====
 ; Unrolled loop of column tile writes. Call one of the addresses stored in the
 ; tilemap._loadColumnLookup lookup table to start from a given row. The loop
 ; will wrap back to 0 after the 28th tile is written and continue until all
@@ -697,137 +809,6 @@
             .dw tilemap._loadColumn + (tilemap._loadColumn_loopSizeBytes * row)
         .endr
 .ends
-
-;====
-; Jumps to the relevant label if a column scroll is needed after a call to
-; tilemap.adjustXPixels. Must be called with either 3 arguments (left, right, else)
-; or just else alone
-;
-; Example usage:
-;
-; tilemap.ifColScroll +
-;     ; Column is scrolling (left or right)
-; +:
-;
-; tilemap.ifColScroll left, right, +
-;     left:
-;         ; Handle left scroll
-;         jp + ; skip right label
-;     right:
-;         ; Handle right scroll
-; +:
-;
-; @in   left    (optional) continue to this label if left column needs loading
-; @in   right   (optional) jump to this label if the right column needs loading
-; @in   else    jump to this label if no columns need loading
-;====
-.macro "tilemap.ifColScroll" args left, right, else
-    .if NARGS == 1
-        ; Only one argument passed ('else' label)
-        ld a, (tilemap.ram.flags)               ; load flags
-        and tilemap.X_SCROLL_RESET_MASK ~ $ff   ; remove other flags (negate reset mask)
-        jp z, \1                                ; jp to else label if no column to scroll
-        ; ...otherwise continue
-    .elif NARGS == 3
-        ; 3 arguments passed (left, right, else)
-        ld a, (tilemap.ram.flags)               ; load flags
-        and tilemap.X_SCROLL_RESET_MASK ~ $ff   ; remove other flags (negate reset mask)
-        jp z, else                              ; no column to scroll
-
-        bit tilemap.SCROLL_RIGHT_PENDING_BIT, a ; check right scroll flag
-        jp nz, right                            ; jp if scrolling right
-        ; ...otherwise continue to left label
-    .else
-        .print "\ntilemap.ifColScroll requires 1 or 3 arguments (left/right/else, or just else alone)\n\n"
-        .fail
-    .endif
-.endm
-
-;====
-; Jumps to the relevant label if a row scroll is needed after a call to
-; tilemap.adjustXPixels. Must be called with either 3 arguments (up, down, else)
-; or just else alone
-;
-; Example usage:
-;
-; tilemap.ifRowScroll +
-;     ; Row is scrolling (up or down)
-; +:
-;
-; tilemap.ifRowScroll up, down, +
-;     up:
-;         ; Handle up scroll
-;         jp + ; skip down label
-;     down:
-;         ; Handle down scroll
-; +:
-;
-; @in   up      (optional) continue to this label if top row needs loading
-; @in   down    (optional) jump to this label if the bottom row needs loading
-; @in   else    jump to this label if no columns need loading
-;====
-.macro "tilemap.ifRowScroll" args up, down, else
-    .if NARGS == 1
-        ; Only one argument passed ('else' label)
-        ld a, (tilemap.ram.flags)               ; load flags
-        and tilemap.Y_SCROLL_RESET_MASK ~ $ff   ; remove other flags (negate reset mask)
-        jp z, \1                                ; jp to else label if no row to scroll
-        ; ...otherwise continue
-    .elif NARGS == 3
-        ld a, (tilemap.ram.flags)
-        and tilemap.Y_SCROLL_RESET_MASK ~ $ff   ; remove other flags (negate reset mask)
-        jp z, else                              ; no row to scroll
-
-        bit tilemap.SCROLL_DOWN_PENDING_BIT, a
-        jp nz, down                             ; scroll down
-        ; ...otherwise continue to 'up' label
-    .else
-        .print "\ntilemap.ifRowScroll requires 1 or 3 arguments (up/down/else, or just else alone)\n\n"
-        .fail
-    .endif
-.endm
-
-;====
-; Sends the buffered scroll register values to the VDP. This should be called
-; when the display is off or during a V or H interrupt
-;====
-.macro "tilemap.updateScrollRegisters"
-    ld a, (tilemap.ram.xScrollBuffer)
-    utils.vdp.setRegister utils.vdp.SCROLL_X_REGISTER
-
-    ld a, (tilemap.ram.yScrollBuffer)
-    utils.vdp.setRegister utils.vdp.SCROLL_Y_REGISTER
-.endm
-
-;====
-; Sets the VRAM write address to the row that needs scrolling. If no row needs
-; scrolling will just set the address to the last row scrolled
-;
-; @out  VRAM write address  write address for the row (column 0)
-; @out  c                   VDP data port
-;====
-.macro "tilemap.setRowScrollSlot"
-    ld hl, (tilemap.ram.vramRowWrite)
-    utils.vdp.prepWriteHL
-.endm
-
-;====
-; Set the VRAM write address to the column
-;====
-.macro "tilemap.loadScrollCol"
-    ; Get X column offset
-    ld a, (tilemap.ram.xScrollBuffer)   ; load X scroll
-    neg                                 ; negate
-    rrca                                ; divide by 2
-    rrca                                ; divide by 2 (4)
-    and %00111110                       ; clean value
-    ld e, a                             ; load result into E
-
-    ld c, tilemap.VDP_DATA_PORT         ; data port
-    ld b, tilemap.COL_SIZE_BYTES        ; bytes to write
-    ld iy, (tilemap.ram.colWriteCall)   ; load call address in tilemap._loadColumn
-    call tilemap._callIY                ; call address
-.endm
 
 ;====
 ; Calls the address stored in IY
