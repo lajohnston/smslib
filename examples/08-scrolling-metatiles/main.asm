9ac29d145493aaac0e9b4e38d08ab83acb096dab
Lee Johnston <lajohnston@live.co.uk>
2022-11-19 17:21:16 +0000

    scroll.metatiles: Add example

---
+++ examples/08-scrolling-metatiles/main.asm
@@ -0,0 +1,159 @@
+;====
+; SMSLib scrolling metatilemap example
+;
+; Like the previous example we will have a tilemap that is larger than the
+; screen and can be scrolled using the directional input. However, this time
+; we will use the scroll/metatiles.asm handler which will build the tilemap
+; using 'metatiles', tiles that are grouped together and referred to with a
+; 1-byte reference. This shrinks the size down considerably (a 2x2 metatile
+; would usually use 8 bytes, so 8x more) and the tilemap becomes small enough
+; to be stored uncompressed and modified in RAM.
+;====
+.sdsctag 1.10, "smslib metatile scrolling", "smslib metatile scrolling example", "lajohnston"
+
+;====
+; Import smslib
+;====
+.define interrupts.handleVBlank 1   ; enable VBlanks (see VBlank example)
+.incdir "../../"                    ; point to smslib directory
+.include "smslib.asm"
+
+; Set the size (each value can be 2, 4, 8, 16)
+.define scroll.metatiles.COLS_PER_METATILE 4
+.define scroll.metatiles.ROWS_PER_METATILE 4
+.include "scroll/metatiles.asm"     ; a metatile scroll handler
+
+.incdir "."                         ; point back to current working directory
+
+;====
+; Define asset data
+;====
+
+; Scroll speed in pixels per axis (should be between 0 and 8 inclusive)
+.define SCROLL_SPEED 2
+
+;====
+; A set of metatile definitions consisting of raw tile data stored sequentally.
+; Ensure these are aligned to the scroll.metatiles.DEFS_ALIGN value.
+;====
+.section "metatileDefs" free align scroll.metatiles.DEFS_ALIGN
+    metatileDefs:
+        .include "../assets/metatiles/metatileDefs.asm"
+.ends
+
+;====
+; A map of metatile references. Each ref is a byte (0-255) and refers to one
+; of the metatile definitions specified above.
+;
+; Here we programmatically generate the map but a real game may have a designed
+; map that's compressed on the ROM.
+;====
+.section "metatileReference" free
+    metatileMap:
+        .repeat 64 index row
+            .repeat 64 index col
+                .if row == 0 || col == 0 || row == 63 || col == 64
+                    ; Border around the edges
+                    .db 5   ; metatile #5
+                .else
+                    ; Diagonal stripes in middle
+                    .db ((row + col) # 4)
+                .endif
+            .endr
+        .endr
+    metatileMapEnd:
+.ends
+
+;====
+; Palette and patterns
+;====
+.section "assets" free
+    paletteData:
+        .incbin "../assets/metatiles/palette.bin" fsize paletteDataSize
+
+    patternData:
+        .incbin "../assets/metatiles/patterns.bin" fsize patternDataSize
+.ends
+
+;====
+; Initialise program
+;====
+.section "init" free
+    ; SMSLib will jump to the init label after booting the system
+    init:
+        ; Load palette and patterns
+        palette.setSlot 0
+        palette.load paletteData, paletteDataSize
+        patterns.setSlot 0
+        patterns.load patternData, patternDataSize
+
+        ;===
+        ; Load the metatile map into RAM
+        ; An actual game would have compressed data that it decompresses to RAM
+        ; but for this example we'll just copy it from ROM to RAM
+        ;===
+        ld hl, metatileMap                  ; set HL to the map in ROM
+        scroll.metatiles.loadDEMap          ; set DE to the map in RAM
+        ld bc, metatileMapEnd - metatileMap ; number of bytes to write
+        ldir                                ; copy from (HL) to (DE) until BC is 0
+
+        ; Set the location of the metatile definitions
+        ld hl, metatileDefs
+        scroll.metatiles.setDefs
+
+        ; Initialise map (offset x0, y0)
+        ld a, scroll.metatiles.WIDTH_64     ; full map width in metatiles
+        ld d, 0                             ; metatile col offset
+        ld e, 0                             ; metatile row offset
+        scroll.metatiles.init               ; draw the inital screen
+
+        ; Enable the display
+        vdp.startBatch
+            vdp.enableDisplay
+            vdp.enableVBlank
+
+            ; Left column gets filled with junk when scrolling, so best hide it
+            vdp.hideLeftColumn
+        vdp.endBatch
+
+        ; Enable interrupts then start the update loop
+        interrupts.enable
+        jp update
+.ends
+
+;====
+; The update loop that runs each frame during the active display
+;====
+.section "update" free
+    update:
+        ; Wait for the next VBlank to finish
+        interrupts.waitForVBlank
+
+        ;===
+        ; Adjust tilemap based on joypad input direction
+        ;===
+        input.readPort1                 ; read the state of joypad 1
+
+        input.loadADirX SCROLL_SPEED    ; load A with X direction * scroll speed
+        scroll.metatiles.adjustXPixels  ; adjust tilemap X by that many pixels
+
+        input.loadADirY SCROLL_SPEED    ; load A with Y direction * scroll speed
+        scroll.metatiles.adjustYPixels  ; adjust tilemap Y by that many pixels
+
+        ; Update RAM buffers (doesn't update VDP/VRAM yet)
+        scroll.metatiles.update
+
+        jp update   ; start loop again
+.ends
+
+;====
+; The VBlank routine where we'll apply changes to the VDP
+;====
+.section "render" free
+    interrupts.onVBlank:
+        ; Apply scroll changes to the VDP/VRAM
+        scroll.metatiles.render
+
+        ; Mark the end of the VBlank handler
+        interrupts.endVBlank
+.ends
---
+++ examples/assets/metatiles/metatileDefs.asm
@@ -0,0 +1,70 @@
+;====
+; Programmatically creates some metatiles definitions according to the size,
+; to demonstrate the flexible scroll.metatiles.ROWS_PER_METATILE and
+; scroll.metatiles.COLS_PER_METATILE values you can set. Set these in the
+; metatile scrolling example before including the scroll/metatiles.asm file.
+;
+; Each metatile definition is square or rectangle with its own color.
+;
+; Note how
+;====
+
+; The number of metatiles definitions/colors
+.define colors = 6
+
+.if scroll.metatiles.ROWS_PER_METATILE == 2 && scroll.metatiles.COLS_PER_METATILE == 2
+    ; 2x2 metatiles just require four corners
+    .repeat colors index color
+        .redefine cornerPattern color * 4
+
+        tilemap.tile cornerPattern
+        tilemap.tile cornerPattern tilemap.FLIP_X
+        tilemap.tile cornerPattern tilemap.FLIP_Y
+        tilemap.tile cornerPattern tilemap.FLIP_XY
+    .endr
+.else
+    ; Larger metatiles require corners, side, and middle
+    .repeat colors index color
+        ; Pattern numbers that define the corners, sides and middle
+        .redefine basePattern      color * 4            ; the first pattern for this color
+        .redefine cornerPattern    basePattern          ; corner outline
+        .redefine sideEdgePattern  basePattern + 1      ; side outline (vertical line)
+        .redefine topEdgePattern   basePattern + 2      ; top outline (horizontal line)
+        .redefine middlePattern    basePattern + 3      ; plain tile without an outline
+
+        ;===
+        ; Top row
+        ;===
+        tilemap.tile cornerPattern                          ; top left corner
+
+        .repeat scroll.metatiles.COLS_PER_METATILE - 2
+            tilemap.tile topEdgePattern                     ; top edge
+        .endr
+
+        tilemap.tile cornerPattern tilemap.FLIP_X           ; top right corner
+
+        ;===
+        ; Middle rows
+        ;===
+        .repeat scroll.metatiles.ROWS_PER_METATILE - 2
+            tilemap.tile sideEdgePattern                    ; left edge
+
+            .repeat scroll.metatiles.COLS_PER_METATILE - 2
+                tilemap.tile middlePattern                  ; middle
+            .endr
+
+            tilemap.tile sideEdgePattern tilemap.FLIP_X     ; right edge
+        .endr
+
+        ;===
+        ; Last row
+        ;===
+        tilemap.tile cornerPattern tilemap.FLIP_Y           ; bottom left corner
+
+        .repeat scroll.metatiles.COLS_PER_METATILE - 2
+            tilemap.tile topEdgePattern tilemap.FLIP_Y      ; bottom middle edge
+        .endr
+
+        tilemap.tile cornerPattern tilemap.FLIP_XY          ; bottom right corner
+    .endr
+.endif
