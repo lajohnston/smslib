8b481955f1f8ec5e6937b16ed2ed11e1c486ce90
Lee Johnston <lajohnston@live.co.uk>
2023-05-07 14:46:51 +0100

    palette.asm: Add assertions to macro arguments

--- palette.asm
+++ palette.asm
@@ -23,6 +23,10 @@
     .include "utils/outiBlock.asm"
 .endif
 
+.ifndef utils.assert
+    .include "utils/assert.asm"
+.endif
+
 ;====
 ; Procedures
 ;====
@@ -32,6 +36,11 @@
 ; to the nearest 85 (0, 85, 170, 255)
 ;====
 .macro "palette.rgb" args red green blue
+    utils.assert.equals NARGS, 3, "palette.asm \. received the wrong number of arguments"
+    utils.assert.number red, "palette.asm \.: Invalid red argument"
+    utils.assert.number green, "palette.asm \.: Invalid green argument"
+    utils.assert.number blue, "palette.asm \.: Invalid blue argument"
+
     ; Round to nearest 85 then AND with $ff to calculate floor
     .define \.\@red = (red + 42.5) / 85 & $ff
     .define \.\@green = (green + 42.5) / 85 & $ff
@@ -50,6 +59,11 @@
 ; @in   blue    blue value
 ;====
 .macro "palette.writeRgb" args red, green, blue
+    utils.assert.equals NARGS, 3, "palette.asm \. received the wrong number of arguments"
+    utils.assert.number red, "palette.asm \.: Invalid red argument"
+    utils.assert.number green, "palette.asm \.: Invalid green argument"
+    utils.assert.number blue, "palette.asm \.: Invalid blue argument"
+
     ; Round to nearest 85 then AND with $ff to calculate floor
     .define \.\@red = (red + 42.5) / 85 & $ff
     .define \.\@green = (green + 42.5) / 85 & $ff
@@ -70,6 +84,10 @@
 ;               so long as this label is defined before this macro is called.
 ;====
 .macro "palette.writeBytes" args address size
+    utils.assert.equals NARGS, 2, "palette.asm \. received the wrong number of arguments"
+    utils.assert.label address, "palette.asm \.: Invalid address argument"
+    utils.assert.number size, "palette.asm \.: Invalid size argument"
+
     ld hl, address
     utils.outiBlock.write size
 .endm
@@ -83,10 +101,16 @@
 ; @in       [offset=0]  how many colors to skip from the start of the data
 ;====
 .macro "palette.writeSlice" args dataAddr count offset
-    .ifndef offset
-        utils.outiBlock.writeSlice dataAddr palette.ELEMENT_SIZE_BYTES count 0
+    utils.assert.label dataAddr, "palette.asm \.: Invalid label argument"
+    utils.assert.number count, "palette.asm \.: Invalid count argument"
+
+    .ifdef offset
+        utils.assert.equals NARGS, 3, "palette.asm \. received the wrong number of arguments"
+        utils.assert.number offset, "palette.asm \.: Expected count to be a numeric value"
+        utils.outiBlock.writeSlice dataAddr, palette.ELEMENT_SIZE_BYTES, count, offset
     .else
-        utils.outiBlock.writeSlice dataAddr palette.ELEMENT_SIZE_BYTES count offset
+        utils.assert.equals NARGS, 2, "palette.asm \. received the wrong number of arguments"
+        utils.outiBlock.writeSlice dataAddr, palette.ELEMENT_SIZE_BYTES, count, 0
     .endif
 .endm
 
@@ -96,5 +120,8 @@
 ; @in   index   the palette index (0-31)
 ;====
 .macro "palette.setIndex" args index
+    utils.assert.equals NARGS, 1, "palette.asm \. received the wrong number of arguments"
+    utils.assert.range index, 0, 31, "palette.asm \.: Invalid index argument"
+
     utils.vdp.prepWrite (palette.VRAM_ADDR + index)
 .endm
