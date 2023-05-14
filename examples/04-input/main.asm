5bcf5b42d6835e94d256a929e2b6805678a81f04
Lee Johnston <lajohnston@live.co.uk>
2023-05-14 21:00:19 +0100

    input: Re-work example to use new press/held checks

--- examples/04-input/main.asm
+++ examples/04-input/main.asm
@@ -1,14 +1,14 @@
 ;====
 ; SMSLib Joypad Input Example
 ;
-; This demo detects user input and displays the result on the screen. Renders
-; text on the screen stating which buttons are being pressed
+; This demo detects user input (pressed, current and held) and displays the
+; result in a table on the screen
 ;====
 .sdsctag 1.10, "smslib input", "smslib input tutorial", "lajohnston"
 
 ; Import smslib
 .define interrupts.handleVBlank 1   ; enable VBlank handling in interrupts.asm
-.incdir "../../"                    ; back to smslib directory
+.incdir "../../"                    ; point to smslib directory
 .include "smslib.asm"               ; base library
 .incdir "."                         ; return to current directory
 
@@ -29,33 +29,40 @@
     fontPatterns:
         .incbin "../assets/font.bin" fsize fontPatternsSize
 
-    ; Ascii for -1, 0, 1, padded to 2 chars to keep alignment
-    asciiMinusOne:
-        .asc "-1"
-
-    asciiZero:
-        .asc " 0"
-
-    asciiOne:
-        .asc " 1"
-
-    ; Template string. We'll populate the values based on the buttons pressed
+    ; Table template. We'll update this each frame
     template:
-        .asc "         Dir X    :  0          "
-        .asc "         Dir Y    :  0          "
+        .asc "          Pressed Current Held  "
         .asc "                                "
-        .asc "         Button 1 :  0          "
-        .asc "         Button 2 :  0          "
+        .asc " Up       ( )     ( )     ( )   "
+        .asc " Down     ( )     ( )     ( )   "
+        .asc " Left     ( )     ( )     ( )   "
+        .asc " Right    ( )     ( )     ( )   "
+        .asc "                                "
+        .asc " Button 1 ( )     ( )     ( )   "
+        .asc " Button 2 ( )     ( )     ( )   "
         .db $ff ; terminator
+
+    ; We'll add an asterisk in between the brackets in the template string,
+    ; indicating which condition has been met
+    asciiAsterisk:
+        .asc '*'
 .ends
 
-; Locations (columns, rows) of the values in the template text
-.define VALUE_COLUMN = 20
-.define ROW_OFFSET = 9
-.define DIR_X_ROW = ROW_OFFSET
-.define DIR_Y_ROW = ROW_OFFSET + 1
-.define BUTTON_1_ROW = ROW_OFFSET + 3
-.define BUTTON_2_ROW = ROW_OFFSET + 4
+; The starting row the render the table from
+.define TABLE_ROW_OFFSET = 7
+
+; The indicator tile columns for each condition (Pressed, Current, Held)
+.define PRESSED_INDICATOR_COLUMN = 11
+.define CURRENT_INDICATOR_COLUMN = PRESSED_INDICATOR_COLUMN + 8
+.define HELD_INDICATOR_COLUMN = CURRENT_INDICATOR_COLUMN + 8
+
+; The row number for each button
+.define UP_ROW = TABLE_ROW_OFFSET + 2
+.define DOWN_ROW = TABLE_ROW_OFFSET + 3
+.define LEFT_ROW = TABLE_ROW_OFFSET + 4
+.define RIGHT_ROW = TABLE_ROW_OFFSET + 5
+.define BUTTON_1_ROW = TABLE_ROW_OFFSET + 7
+.define BUTTON_2_ROW = TABLE_ROW_OFFSET + 8
 
 ;====
 ; Initialise the example
@@ -68,9 +75,6 @@
         patterns.setIndex 0
         patterns.writeBytes fontPatterns, fontPatternsSize
 
-        tilemap.setColRow 0, ROW_OFFSET
-        tilemap.writeBytesUntil $ff, template
-
         vdp.startBatch
             vdp.enableDisplay
             vdp.enableVBlank
@@ -78,76 +82,168 @@
 
         interrupts.enable
 
-        ; Stop program for the time being
-        - jp -
+        -:
+            halt    ; wait for next VBlank
+        jp -        ; infinite loop
 .ends
 
-.section "render" free
-    ; interrupts.onVBlank is called by interrupts.asm 50/60x a second (PAL/NTSC)
-    ; We'll check the input each time and update the display accordingly
+;====
+; interrupts.onVBlank is called by interrupts.asm 50/60x a second (PAL/NTSC)
+; We'll check the input each time and update the display accordingly
+; (see vblank example)
+;====
+.section "vBlank" free
     interrupts.onVBlank:
         ; Read input from port 1
         ; You can also try this with input.readPort2
         input.readPort1
 
-        ;==
-        ; Update X direction
-        ;==
-        tilemap.setColRow VALUE_COLUMN, DIR_X_ROW
-        tilemap.writeBytes asciiZero, 2         ; set value to 0 by default
+        ; Redraw table with blank values
+        call drawBlankTable
 
-        ; If left is being pressed
-        input.if input.LEFT, +
-            tilemap.setColRow VALUE_COLUMN, DIR_X_ROW
-            tilemap.writeBytes asciiMinusOne, 2 ; set to -1
-        +:
-
-        ; If right is being pressed
-        input.if input.RIGHT, +
-            tilemap.setColRow VALUE_COLUMN, DIR_X_ROW
-            tilemap.writeBytes asciiOne, 2     ; set to 1
-        +:
-
-        ;==
-        ; Update Y direction
-        ;==
-        tilemap.setColRow VALUE_COLUMN, DIR_Y_ROW
-        tilemap.writeBytes asciiZero, 2        ; set value to 0 by default
-
-        ; If up is being pressed
-        input.if input.UP, +
-            tilemap.setColRow VALUE_COLUMN, DIR_Y_ROW
-            tilemap.writeBytes asciiMinusOne, 2; set to -1
-        +:
-
-        ; If down is being pressed
-        input.if input.DOWN, +
-            tilemap.setColRow VALUE_COLUMN, DIR_Y_ROW
-            tilemap.writeBytes asciiOne, 2      ; set to 1
-        +:
-
-        ;==
-        ; Update Button 1
-        ;==
-        tilemap.setColRow VALUE_COLUMN, BUTTON_1_ROW
-        tilemap.writeBytes asciiZero, 2         ; set value to 0 by default
-
-        input.if input.BUTTON_1, +
-            tilemap.setColRow VALUE_COLUMN, BUTTON_1_ROW
-            tilemap.writeBytes asciiOne, 2      ; set to 1
-        +:
-
-        ;==
-        ; Update Button 2
-        ;==
-        tilemap.setColRow VALUE_COLUMN, BUTTON_2_ROW
-        tilemap.writeBytes asciiZero, 2         ; set value to 0 by default
-
-        input.if input.BUTTON_2, +
-            tilemap.setColRow VALUE_COLUMN, BUTTON_2_ROW
-            tilemap.writeBytes asciiOne, 2      ; set to 1
-        +:
+        ;===
+        ; Update the columns to show which of the following condition(s)
+        ; applies to each button
+        ;===
+        call detectPressed  ; indicate buttons that have just been pressed this frame
+        call detectCurrent  ; indicate buttons that are currently pressed
+        call detectHeld     ; indicate buttons that were pressed last frame and this frame
 
         ; End VBlank handler
         interrupts.endVBlank
+.ends
+
+;====
+; Draws an ascii asterisk in the given column and row
+;====
+.macro "writeAsterisk" args column row
+    tilemap.setColRow column, row
+    tilemap.writeBytes asciiAsterisk 1
+.endm
+
+;====
+; Draws the blank table with none of the indicators populated
+;====
+.section "drawBlankTable" free
+    drawBlankTable:
+        tilemap.setColRow 0, TABLE_ROW_OFFSET
+        tilemap.writeBytesUntil $ff, template
+        ret
+.ends
+
+;====
+; Uses the if..pressed macros to detect when a button has just been pressed
+; this frame, and indicate this with a '*' character. This only occurs for
+; 1 frame so the indicator will flash once each time the button is pressed
+;====
+.section "detectPressed" free
+    detectPressed:
+        ; Check if either UP or DOWN have just been pressed
+        input.ifYDirPressed _up, _down, +
+            _up:
+                writeAsterisk PRESSED_INDICATOR_COLUMN, UP_ROW
+                jp +
+            _down:
+                writeAsterisk PRESSED_INDICATOR_COLUMN, DOWN_ROW
+        +:
+
+        ; Check if either LEFT or RIGHT have just been pressed
+        input.ifXDirPressed _left, _right, +
+            _left:
+                writeAsterisk PRESSED_INDICATOR_COLUMN, LEFT_ROW
+                jp +
+            _right:
+                writeAsterisk PRESSED_INDICATOR_COLUMN, RIGHT_ROW
+        +:
+
+        ; Check if BUTTON 1 has just been pressed
+        input.ifPressed input.BUTTON_1, +
+            writeAsterisk PRESSED_INDICATOR_COLUMN, BUTTON_1_ROW
+        +:
+
+        ; Check if BUTTON 2 has just been pressed
+        input.ifPressed input.BUTTON_2, +
+            writeAsterisk PRESSED_INDICATOR_COLUMN, BUTTON_2_ROW
+        +:
+
+        ret
+.ends
+
+;====
+; Uses the 'if' macros to detect when a button is currently pressed, and
+; indicate this with a '*' character. Unlike the 'pressed' condition this
+; indicator will stay on until the button is released
+;====
+.section "detectCurrent" free
+    detectCurrent:
+        ; Check if either UP or DOWN are currently pressed down
+        input.ifYDir _up, _down, +
+            _up:
+                writeAsterisk CURRENT_INDICATOR_COLUMN, UP_ROW
+                jp +
+            _down:
+                writeAsterisk CURRENT_INDICATOR_COLUMN, DOWN_ROW
+        +:
+
+        ; Check if either LEFT or RIGHT are currently pressed down
+        input.ifXDir _left, _right, +
+            _left:
+                writeAsterisk CURRENT_INDICATOR_COLUMN, LEFT_ROW
+                jp +
+            _right:
+                writeAsterisk CURRENT_INDICATOR_COLUMN, RIGHT_ROW
+        +:
+
+        ; Check if BUTTON_1 is currently pressed down
+        input.if input.BUTTON_1, +
+            writeAsterisk CURRENT_INDICATOR_COLUMN, BUTTON_1_ROW
+        +:
+
+        ; Check if BUTTON_2 is currently pressed down
+        input.if input.BUTTON_2, +
+            writeAsterisk CURRENT_INDICATOR_COLUMN, BUTTON_2_ROW
+        +:
+
+        ret
+.ends
+
+;====
+; Uses the if..held macros to detect when a button is currently pressed and
+; was also pressed in the last frame, then indicate this with a '*' character.
+;
+; If you tap the button fast enough you'll see that the 'pressed' and 'current'
+; indicators highlight but not the 'held' indicator. From then on the held
+; indicator will stay on until the button is released
+;====
+.section "detectHeld" free
+    detectHeld:
+        ; Check if either UP or DOWN are currently held
+        input.ifYDirHeld _up, _down, +
+            _up:
+                writeAsterisk HELD_INDICATOR_COLUMN, UP_ROW
+                jp +
+            _down:
+                writeAsterisk HELD_INDICATOR_COLUMN, DOWN_ROW
+        +:
+
+        ; Check if either LEFT or RIGHT are currently held
+        input.ifXDirHeld _left, _right, +
+            _left:
+                writeAsterisk HELD_INDICATOR_COLUMN, LEFT_ROW
+                jp +
+            _right:
+                writeAsterisk HELD_INDICATOR_COLUMN, RIGHT_ROW
+        +:
+
+        ; Check if BUTTON_1 is currently held
+        input.ifHeld input.BUTTON_1, +
+            writeAsterisk HELD_INDICATOR_COLUMN, BUTTON_1_ROW
+        +:
+
+        ; Check if BUTTON_2 is currently held
+        input.ifHeld input.BUTTON_2, +
+            writeAsterisk HELD_INDICATOR_COLUMN, BUTTON_2_ROW
+        +:
+
+        ret
 .ends
