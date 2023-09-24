2a611cd8cac6f2e376cf393afad163ed7e3eb353
Lee Johnston <lajohnston@live.co.uk>
2023-09-24 19:48:25 +0100

    init: Only initialise modules if .ENABLED == 1

--- init.asm
+++ init.asm
@@ -59,32 +59,44 @@
 .macro "init.smslibModules"
     ; initialise paging registers
     .ifdef mapper.ENABLED
-        mapper.init
+        .ifeq mapper.ENABLED 1
+            mapper.init
+        .endif
     .endif
 
     ; initialise vdp registers
     .ifdef vdp.ENABLED
-        vdp.init
+        .ifeq vdp.ENABLED 1
+            vdp.init
+        .endif
     .endif
 
     ; initialise pause handler
     .ifdef pause.ENABLED
-        pause.init
+        .ifeq pause.ENABLED 1
+            pause.init
+        .endif
     .endif
 
     ; initialise sprite buffer
     .ifdef sprites.ENABLED
-        sprites.init
+        .ifeq sprites.ENABLED 1
+            sprites.init
+        .endif
     .endif
 
     ; initialise input handler
     .ifdef input.ENABLED
-        input.init
+        .ifeq input.ENABLED 1
+            input.init
+        .endif
     .endif
 
     ; initialise interrupt handler
     .ifdef interrupts.ENABLED
-        interrupts.init
+        .ifeq interrupts.ENABLED 1
+            interrupts.init
+        .endif
     .endif
 .endm
 
